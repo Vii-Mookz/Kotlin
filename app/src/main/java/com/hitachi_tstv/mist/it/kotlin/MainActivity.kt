8ff9649f241f88809ff9742f805fbfb13a6162dd
Vipavee <vi_vpk_4ever@hotmail.com>
2018-03-29 14:07:18 +0700

    Initial commit

---
+++ app/src/main/java/com/hitachi_tstv/mist/it/kotlin/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.hitachi_tstv.mist.it.kotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
